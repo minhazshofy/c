ddae914e036a6a23fa72fd1a0a9e4faf46235aff
root <root@localhost>
2025-09-04 20:12:13 +0600

    auto publish 1756995133643

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-void main() {
-     char letter = 'C';
-     printf("%c", letter);
- }
+void main()
+{
+    int age;
+    printf("Please enter your age:");
+    scanf("%d", &age);  
+    printf("Your age is, %d", age);
+}
